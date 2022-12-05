4f10dd4e2fd2f3f6b913ee45deff7defbf2616ec
Björn Gylling <bjorn.gylling@gmail.com>
2022-12-05 08:45:22 +0100

    refactor(05): use split_off instead of loop

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -32,10 +32,11 @@ pub fn part_one(input: &str) -> Option<String> {
             break;
         }
         let cap = re.captures(l.unwrap()).unwrap();
-        for _ in 0..to_usize(cap.get(1)) {
-            let c = stacks[to_usize(cap.get(2)) - 1].pop().unwrap();
-            stacks[to_usize(cap.get(3)) - 1].push(c);
-        }
+        let s = to_usize(cap.get(2)) - 1;
+        let m = stacks[s].len() - to_usize(cap.get(1));
+        let mut t = stacks[s].split_off(m);
+        t.reverse();
+        stacks[to_usize(cap.get(3)) - 1].extend(t);
     }
     let s = stacks.iter().map(|s| s.last().unwrap()).collect();
     Some(s)
@@ -69,11 +70,9 @@ pub fn part_two(input: &str) -> Option<String> {
             break;
         }
         let cap = re.captures(l.unwrap()).unwrap();
-        let mut t: Vec<char> = vec![];
-        for _ in 0..to_usize(cap.get(1)) {
-            t.push(stacks[to_usize(cap.get(2)) - 1].pop().unwrap());
-        }
-        t.reverse();
+        let s = to_usize(cap.get(2)) - 1;
+        let m = stacks[s].len() - to_usize(cap.get(1));
+        let t = stacks[s].split_off(m);
         stacks[to_usize(cap.get(3)) - 1].extend(t);
     }
     let s = stacks.iter().map(|s| s.last().unwrap()).collect();
